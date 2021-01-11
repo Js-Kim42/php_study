fbf634367912cf22dca13770257f519b1a94fc1d
Js-Kim42 <outfox100@naver.com>
2021-01-11 19:40:21 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+    <title>Corona News</title>
+    <style>
+        .list-container {
+            display: flex;
+            flex-direction: column;
+            width: 50%;
+            border-radius: 4px;
+            border: 1px solid #eaeaea;
+        }
+
+        h1 {
+            padding: 16px 24px;
+            margin: 0;
+            border-bottom: 1px solid #eaeaea;
+        }
+
+        #list {
+            margin: 32px 0;
+            list-style: none;
+        }
+
+        .list-item {
+            border-bottom: 1px solid #eaeaea;
+            margin-right: 32px;
+            padding: 16px 0;
+            font-size: 14px;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+    </style>
+</head>
+<body>
+<div class="list-container">
+    <h1>코로나 뉴스</h1>
+    <ul id="list">
+        <li class="list-item"><a href="#">코로나 뉴스 첫번째</li>
+        <li class="list-item">코로나 뉴스 두번째</li>
+        <li class="list-item">코로나 뉴스 세번째</li>
+    </ul>
+</div>
+</body>
+</html>
